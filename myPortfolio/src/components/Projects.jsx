08d23e2b546f4bac93e5c6f3570bd7e8d4f42e00
Theo <theo.osinfolarin@gmail.com>
2024-05-28 21:14:14 +0100

    projects

---
+++ myPortfolio/src/components/Projects.jsx
@@ -0,0 +1,18 @@
+const projects = () => {
+
+    <div>
+    <h1>This is a page about my projects</h1>
+
+            <h3>This is a physiotherapy website created using purely HTML and CSS and minimal use of JavaScript for the dropdowns. It was my first project</h3>
+            <div className="ProjectImage">
+                <img src="src/images/Physio4You.png" alt="Physio4You"/>
+            </div>
+
+            <h3>This is a driving school website which uses React, JSX</h3>
+            <div className="ProjectImage"> 
+                <img src="src/images/TrinityDriving.png" alt="TrinityDriving"/>
+            </div>
+</div>
+}
+
+export default projects;
--- myPortfolio/src/components/ProjectsPage.jsx
+++ myPortfolio/src/components/ProjectsPage.jsx
@@ -4,17 +4,7 @@ const ProjectsPage = () => {
     return (  
         <div>
             <Navbar/>
-            <h1>This is a page about my projects</h1>
-
-            <h3>This is a physiotherapy website created using purely HTML and CSS and minimal use of JavaScript for the dropdowns. It was my first project</h3>
-            <div className="ProjectImage">
-                <img src="src/images/Physio4You.png" alt="Physio4You"/>
-            </div>
-
-            <h3>This is a driving school website which uses React, JSX</h3>
-            <div className="ProjectImage"> 
-                <img src="src/images/TrinityDriving.png" alt="TrinityDriving"/>
-            </div>
+            
         </div>
     );
 }
