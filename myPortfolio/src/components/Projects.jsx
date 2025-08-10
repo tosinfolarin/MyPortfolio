b2b1ed8fb7209c498733dfd21866be689a0e77f9
Theo <theo.osinfolarin@gmail.com>
2025-08-10 01:38:34 +0100

    improved visuals of projects

--- myPortfolio/src/components/Projects.jsx
+++ myPortfolio/src/components/Projects.jsx
@@ -5,16 +5,21 @@ const Projects = () => {
     <div className="ProjectsContent">
         <br></br>
         <br></br>
-        <br></br>
+    
  
-            <h3>This is a physiotherapy website created using purely HTML and CSS and minimal use of JavaScript for the dropdowns. It was my first project</h3>
+            <h3 className="ProjectDescription">This is a physiotherapy website created using purely HTML and CSS and minimal use of JavaScript for the dropdowns. It was my first project</h3>
             <div className="ProjectImage">
                 <img src="src/images/Physio4You.png" alt="Physio4You"/>
             </div>
 
-            <h3>This is a driving school website which uses React, JSX</h3>
+            <h3 className="ProjectDescription">This is a driving school website which uses React, JSX</h3>
             <div className="ProjectImage"> 
                 <img src="src/images/TrinityDriving.png" alt="TrinityDriving"/>
+            </div>
+
+            <h3 className="ProjectDescription">This is a physiotherapy website using React, JSX, Node.js, Express.Js</h3>
+            <div className="ProjectImage"> 
+                <img src="src/images/WaitlessPhysio.png" alt="WaitlessPhysio"/>
             </div>
         </div>
     )
