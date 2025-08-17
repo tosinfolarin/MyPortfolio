28e1e979cf26a607da7ad7dfdb3411b8cb17ff87
Theo <theo.osinfolarin@gmail.com>
2025-08-17 21:45:46 +0100

    changed

--- myPortfolio/src/components/Home.jsx
+++ myPortfolio/src/components/Home.jsx
@@ -3,12 +3,10 @@ import information from './information.json';
 const MyInfo = () => {
   return (
     <div className="my-info">
-      {/* Profile picture */}
       <div className="myProfilePic">
         <img src="src/images/ProfilePicture.png" alt="Profile" />
       </div>
 
-      {/* Description */}
       <div className="myDescription">
         <h3 className="name" aria-label="my name">
           {information.greeting}
