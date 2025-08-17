5f4005b44f318029d6138d5047e02c36114e30cb
Theo <theo.osinfolarin@gmail.com>
2025-08-17 21:58:48 +0100

    portfolio links added

--- myPortfolio/src/components/Projects.jsx
+++ myPortfolio/src/components/Projects.jsx
@@ -6,20 +6,39 @@ const Projects = () => {
         <br></br>
         <br></br>
     
- 
+        
             <h3 className="ProjectDescriptionOne">This is a physiotherapy website created using purely HTML and CSS and minimal use of JavaScript for the dropdowns. It was my first project</h3>
             <div className="ProjectImage">
-                <img src="src/images/Physio4You.png" alt="Physio4You"/>
+            <a 
+                // href="none" 
+                // target="_blank"
+                // rel="noopener noreferrer"
+            >
+                <img src="src/images/Physio4You.png" alt="Physio4You" />
+            </a>
             </div>
 
+            
             <h3 className="ProjectDescription">This is a driving school website which uses React, JSX</h3>
             <div className="ProjectImage"> 
+            <a 
+                href="https://trinitydrivingschool.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 <img src="src/images/TrinityDriving.png" alt="TrinityDriving"/>
+            </a>
             </div>
 
             <h3 className="ProjectDescription">This is a physiotherapy website using React, JSX, Node.js, Express.Js</h3>
-            <div className="ProjectImage"> 
+            <div className="ProjectImage">
+            <a 
+                href="https://physioapp-frontend.onrender.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+            > 
                 <img src="src/images/WaitlessPhysio.png" alt="WaitlessPhysio"/>
+            </a>
             </div>
         </div>
     )
