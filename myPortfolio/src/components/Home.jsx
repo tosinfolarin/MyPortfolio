18f49430ebc39dcdb14090aa1612c13697bfda35
Theo <theo.osinfolarin@gmail.com>
2025-08-17 21:45:22 +0100

    changed profile page

--- myPortfolio/src/components/Home.jsx
+++ myPortfolio/src/components/Home.jsx
@@ -3,15 +3,19 @@ import information from './information.json';
 const MyInfo = () => {
   return (
     <div className="my-info">
+      {/* Profile picture */}
       <div className="myProfilePic">
-        <img src="src/images/ProfilePicture.png"/>
+        <img src="src/images/ProfilePicture.png" alt="Profile" />
       </div>
-   
-      <h3 className="name" aria-label="my name">
-        {information.greeting}
-        <br /><br />
-        {information.profile_summary}
-      </h3>
+
+      {/* Description */}
+      <div className="myDescription">
+        <h3 className="name" aria-label="my name">
+          {information.greeting}
+          <br /><br />
+          {information.profile_summary}
+        </h3>
+      </div>
     </div>
   );
 }
