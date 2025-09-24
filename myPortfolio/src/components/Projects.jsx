995040fb5b876e4b14d8ed2e6c2bea1511ad4815
Theo <theo.osinfolarin@gmail.com>
2025-09-24 22:52:51 +0100

    few changes to project order and wording

--- myPortfolio/src/components/Projects.jsx
+++ myPortfolio/src/components/Projects.jsx
@@ -10,19 +10,7 @@ const Projects = () => {
         <br></br>
         <br></br>
     
-        
-            <h3 className="ProjectDescriptionOne"> CLICK THE IMAGE TO EXPLORE THE WEBSITE! <br/>This is a physiotherapy website created using purely HTML and CSS and minimal use of JavaScript for the dropdowns. It was my first project</h3>
-            <div className="ProjectImage">
-            <a 
-                href="https://tosinfolarin.github.io/Physio4You/" 
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <img src={physioWebsitePic} alt="Physio4You" />
-            </a>
-            </div>
-            
-            <h3 className="ProjectDescription"> CLICK THE IMAGE TO EXPLORE THE WEBSITE! <br/>This is a driving school website which uses React.js , Node.js and JSX syntax</h3>
+            <h3 className="ProjectDescriptionOne"> CLICK THE IMAGE TO VISIT THE WEBSITE! <br/>This is a driving school website which uses React.js , Node.js and JSX syntax</h3>
             <div className="ProjectImage"> 
             <a 
                 href="https://trinitydrivingschool.netlify.app/"
@@ -33,7 +21,7 @@ const Projects = () => {
             </a>
             </div>
 
-            <h3 className="ProjectDescription"> CLICK THE IMAGE TO EXPLORE THE WEBSITE! <br/>This is a physiotherapy website using React.js, JSX, Node.js and Express.Js</h3>
+            <h3 className="ProjectDescription"> CLICK THE IMAGE TO VISIT THE WEBSITE! <br/>This is a physiotherapy website using React.js, JSX, Node.js and Express.Js</h3>
             <div className="ProjectImage">
             <a 
                 href="https://physioapp-frontend.onrender.com/"
@@ -43,6 +31,20 @@ const Projects = () => {
                 <img src={ReactPhysioWebsitePic} alt="WaitlessPhysio"/>
             </a>
             </div>
+
+            <h3 className="ProjectDescription"> CLICK THE IMAGE TO VISIT THE WEBSITE! <br/>This is a physiotherapy website created using purely HTML and CSS and minimal use of JavaScript for the dropdowns. It was my first project</h3>
+            
+            <div className="ProjectImage">
+            <a 
+                href="https://tosinfolarin.github.io/Physio4You/" 
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img src={physioWebsitePic} alt="Physio4You" />
+            </a>
+            </div>
+            
+      
         </div>
     )
 }
